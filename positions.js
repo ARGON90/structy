2a4d71e19f1ad0edb85bc3310b33cdff64f75d67
argon <argonglach@gmail.com>
2023-01-23 22:43:21 -0500

    dsa practice

--- positions.js
+++ positions.js
@@ -1,50 +1,35 @@
-class Node {
-  constructor(val) {
-    this.val = val;
-    this.next = null;
+const knightAttack = (n, kr, kc, pr, pc) => {
+  let visited = new Set();
+  let queue = [ [kr, kc, 0] ];
+  let pawnPos = pr + ',' + pc;
+
+  while (queue.length) {
+    let [row, col, len] = queue.shift();
+    let pos = row + ',' + col;
+
+    //inbounds
+    let rowInbounds = 0 <= row && row < n;
+    let colInbounds = 0 <= col && col < n;
+    if (!rowInbounds || !colInbounds) continue;
+
+    //not in visited
+    if (visited.has(pos)) continue;
+    visited.add(pos)
+
+    //takes pawn
+    if (pos === pawnPos) return len;
+
+    let directions = [ [2, 1], [1, 2], [-2, 1], [-1, 2], [-2, -1], [-1, -2], [2, -1], [1, -2] ]
+    for (let direction of directions) {
+      let [rd, cd] = direction;
+      let newRow = row + rd;
+      let newCol = col + cd;
+
+      queue.push([newRow, newCol, len + 1])
+    }
   }
-}
-
-const a = new Node(2);
-const b = new Node(8);
-const c = new Node(3);
-const d = new Node(-1);
-const e = new Node(7);
-
-a.next = b;
-b.next = c;
-c.next = d;
-d.next = e;
-
-// 2 -> 8 -> 3 -> -1 -> 7
-
-// const sumList = (head) => {
-//   let sum = [0];
-//   addSum(sum, head);
-//   return sum;
-// }
-
-// const addSum = (sum, head) => {
-//   if (!head) return;
-//   sum += head.val
-//   console.log(sum)
-//   addSum(sum, head.next)
-// }
-
-const sumList = (head) => {
-  let sum = [0];
-  addSum(sum, head);
-  let total = 0;
-  sum.forEach((num) => total += num)
-  return total;
-}
-
-const addSum = (sum, head) => {
-  if (!head) return;
-  sum.push(head.val)
-  console.log(sum)
-  addSum(sum, head.next)
-}
+  return false;
+};
 
 
-console.log(sumList(a)); // 19
+console.log(knightAttack(8, 1, 1, 2, 2)); // -> 2
