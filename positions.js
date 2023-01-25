4cfff033508e26ec8cd25bd1b51e478b5f7142e5
argon <argonglach@gmail.com>
2023-01-24 23:46:49 -0500

    dsa practice

--- positions.js
+++ positions.js
@@ -1,35 +1,13 @@
-const knightAttack = (n, kr, kc, pr, pc) => {
-  let visited = new Set();
-  let queue = [ [kr, kc, 0] ];
-  let pawnPos = pr + ',' + pc;
+let str = 'hello';
 
-  while (queue.length) {
-    let [row, col, len] = queue.shift();
-    let pos = row + ',' + col;
+function reverseStr(str) {
 
-    //inbounds
-    let rowInbounds = 0 <= row && row < n;
-    let colInbounds = 0 <= col && col < n;
-    if (!rowInbounds || !colInbounds) continue;
+  //base case
+  if (str === '') return '';
 
-    //not in visited
-    if (visited.has(pos)) continue;
-    visited.add(pos)
+  //recursive step
+  return str.slice(-1) + reverseStr(str.slice(0, str.length - 1));
+  //      last letter  +  whatever is returned from reducing str length by 1
 
-    //takes pawn
-    if (pos === pawnPos) return len;
-
-    let directions = [ [2, 1], [1, 2], [-2, 1], [-1, 2], [-2, -1], [-1, -2], [2, -1], [1, -2] ]
-    for (let direction of directions) {
-      let [rd, cd] = direction;
-      let newRow = row + rd;
-      let newCol = col + cd;
-
-      queue.push([newRow, newCol, len + 1])
-    }
-  }
-  return false;
-};
-
-
-console.log(knightAttack(8, 1, 1, 2, 2)); // -> 2
+}
+console.log(reverseStr(str));
