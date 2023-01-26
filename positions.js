7764b0766498b16063523e6500a6810ff85782a3
argon <argonglach@gmail.com>
2023-01-26 17:21:44 -0500

    working on mergesort work

--- positions.js
+++ positions.js
@@ -1,45 +1,13 @@
-// Merge Sort Implentation (Recursion)
-function mergeSort(unsortedArray) {
-  // No need to sort the array if the array only has one element or empty
-  if (unsortedArray.length <= 1) {
-    return unsortedArray;
-  }
+const nonAdjacentSum = (nums, i = 0, memo={}) => {
+  if (i in memo) return memo[i];
+  //i is index of num you're making decision for
+  if (i >= nums.length) return 0;
+  //this means you're done looking at all numbers
 
-  // In order to divide the array in half, we need to figure out the middle
-  const middle = Math.floor(unsortedArray.length / 2);
+  const includesFirstNum = nums[i] + nonAdjacentSum(nums, i + 2, memo);
+  const excludesFirstNum = nonAdjacentSum(nums, i + 1, memo);
 
-  // This is where we will be dividing the array into left and right
-  const left = unsortedArray.slice(0, middle);
-  const right = unsortedArray.slice(middle);
+  memo[i] = Math.max(includesFirstNum, excludesFirstNum);
+  return memo[i];
+};
 
-  // Using recursion to combine the left and right
-  return merge(mergeSort(left), mergeSort(right));
-}
-
-// Merge the two arrays: left and right
-function merge(left, right) {
-  let resultArray = [], leftIndex = 0, rightIndex = 0;
-
-  // We will concatenate values into the resultArray in order
-  while (leftIndex < left.length && rightIndex < right.length) {
-    if (left[leftIndex] < right[rightIndex]) {
-      resultArray.push(left[leftIndex]);
-      leftIndex++; // move left array cursor
-    } else {
-      resultArray.push(right[rightIndex]);
-      rightIndex++; // move right array cursor
-    }
-  }
-
-  // We need to concat here because there will be one element remaining
-  // from either left OR the right
-  let res = resultArray
-    .concat(left.slice(leftIndex))
-    .concat(right.slice(rightIndex));
-    console.log(res)
-  return res;
-}
-
-const array = [10, -1, 2, 5, 0, 6, 4, -5]
-
-console.log('end', mergeSort(array))
