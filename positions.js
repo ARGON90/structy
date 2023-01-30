eb4596dde420a48ec77d054afaf68708987932c3
argon <argonglach@gmail.com>
2023-01-30 07:55:15 -0500

    working on mergesort work

--- positions.js
+++ positions.js
@@ -1,27 +1,41 @@
-const subsets = (elements) => {
-  //base case - answer always includes empty subset
-  //answer will be an array of arrays, so it needs to be nested
-  if (elements.length === 0) return [[]];
-
-  const first = elements[0];
-  const subsetsWithoutFirst = subsets(elements.slice(1));
-  const subsetsWithFirst = [];
-  for (let sub of subsetsWithoutFirst) {
-    subsetsWithFirst.push([first, ...sub]);
+const createCombinations = (items, k) => {
+  // not enough items to support combination length
+  // return an empty array, means you have NO combinations to offer
+  // non-nested [] means you have NO combinations
+  if (k > items.length) {
+    return [];
+  }
+  // target length is 0, return empty result
+  // find combinations of length 0, there's only ONE
+  // combination you can offer, [], so return one combination,
+  // remember it needs to be nested[[]]
+  if (k === 0) {
+    return [[]]
   };
-  console.log(subsetsWithoutFirst, subsetsWithFirst)
-  return [ ...subsetsWithoutFirst, ...subsetsWithFirst ];
+
+  const first = items[0]
+  // logic: exclude the first item in the list, and loop it into other arrays when they've bubbled up
+  // ALSO need to decrease the k, the array size
+
+  // decreasing the k here will help us generate all possibilities of a number
+  // [a, b, c, d] (k3) -> Start with this, take the first letter out
+  // [b, c, d] k(3) -> can only return this one combination whereas
+  // [b, c, d] k(2) -> will return all partial combinations of these letters
+  const partialCombos = createCombinations(items.slice(1), k - 1)
+  const combosWithFirst = [];
+
+  for (let partialCombo of partialCombos) {
+    combosWithFirst.push([ first, ...partialCombo]);
+  }
+
+  const combosWithoutFirst = createCombinations(items.slice(1), k);
+
+  return [...combosWithFirst, ...combosWithoutFirst ];
 };
 
-
-console.log(subsets(['a', 'b', 'c'])); // ->
+console.log(createCombinations(["a", "b", "c", "d"], 3)); // ->
 // [
-//   [],
-//   [ 'c' ],
-//   [ 'b' ],
-//   [ 'b', 'c' ],
-//   [ 'a' ],
-//   [ 'a', 'c' ],
 //   [ 'a', 'b' ],
-//   [ 'a', 'b', 'c' ]
+//   [ 'a', 'c' ],
+//   [ 'b', 'c' ]
 // ]
