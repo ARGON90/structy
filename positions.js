54f7e1b404d9638654d918aad3339c972eb46895
argon <argonglach@gmail.com>
2023-01-31 12:05:45 -0500

    working on DSA

--- positions.js
+++ positions.js
@@ -1,41 +1,24 @@
-const createCombinations = (items, k) => {
-  // not enough items to support combination length
-  // return an empty array, means you have NO combinations to offer
-  // non-nested [] means you have NO combinations
-  if (k > items.length) {
-    return [];
-  }
-  // target length is 0, return empty result
-  // find combinations of length 0, there's only ONE
-  // combination you can offer, [], so return one combination,
-  // remember it needs to be nested[[]]
-  if (k === 0) {
-    return [[]]
-  };
+const getChoices = (s) => {
+  if (!s.length) return '';
+  if (!s.includes('(')) return s[0];
 
-  const first = items[0]
-  // logic: exclude the first item in the list, and loop it into other arrays when they've bubbled up
-  // ALSO need to decrease the k, the array size
+  let res = '';
+  let pushChar = false;
 
-  // decreasing the k here will help us generate all possibilities of a number
-  // [a, b, c, d] (k3) -> Start with this, take the first letter out
-  // [b, c, d] k(3) -> can only return this one combination whereas
-  // [b, c, d] k(2) -> will return all partial combinations of these letters
-  const partialCombos = createCombinations(items.slice(1), k - 1)
-  const combosWithFirst = []
-
-  for (let partialCombo of partialCombos) {
-    combosWithFirst.push([ first, ...partialCombo]);
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === ')') {
+      pushChar = false;
+    }
+    if (pushChar) {
+      res += s[i]
+    }
+    if (s[i] === '(') {
+      pushChar = true
+    }
   }
 
-  const combosWithoutFirst = createCombinations(items.slice(1), k);
+  return res;
 
-  return [...combosWithFirst, ...combosWithoutFirst ];
-};
+}
 
-console.log(createCombinations(["a", "b", "c", "d"], 3)); // ->
-// [
-//   [ 'a', 'b' ],
-//   [ 'a', 'c' ],
-//   [ 'b', 'c' ]
-// ]
+console.log(getChoices('get(choi)ces'))
