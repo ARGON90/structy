dd1b7ca738134481e3902c336283df85eecfdfae
argon <argonglach@gmail.com>
2023-01-31 18:22:25 -0500

    working on DSA

--- positions.js
+++ positions.js
@@ -1,49 +1,41 @@
 class Node {
   constructor(val) {
-    this.val = val
-    this.next = null
-
+    this.val = val;
+    this.next = null;
   }
 }
 
-const a = new Node(3);
-const b = new Node(2);
-const c = new Node(7);
-const d = new Node(7);
-const e = new Node(2);
-const f = new Node(3);
+const a = new Node('a');
+const b = new Node('b');
+const c = new Node('c');
+const d = new Node('d');
 
 a.next = b;
 b.next = c;
 c.next = d;
-d.next = e;
-e.next = f;
-
-// const a = new Node(3);
-// const b = new Node(2);
-// const c = new Node(4);
-
-// a.next = b;
-// b.next = c;
 
 
-// 3 -> 2 -> 7 -> 7 -> 2 -> 3
+const linkedListCycle = (head, set = new Set()) => {
+  if (!head.ne)
+  let slow = head;
 
-const linkedPalindrome = (head) => {
-  const reverse = reverseList(head);
-  const normal = normList(head);
-  console.log(normal, reverse)
-  return normal === reverse
-}
+  let fast = head.next;
 
-function reverseList(head) {
-  if (head === null) return '';
-  return `${reverseList(head.next)}` + `${head.val}`;
-}
-function normList(head) {
-  if (head === null) return '';
-  return `${head.val}` + `${normList(head.next)}`;
-}
+  while (slow !== null && fast.next !== null) {
+    if (slow.val === fast.val) return true;
+    slow = slow.next;
+    fast = fast.next;
+    if (fast.next) {
+      fast = fast.next
+    } else {
+      return false
+    }
+  }
+  return false;
+};
 
+//         _______
+//       /        \
+// a -> b -> c -> d
 
-console.log(linkedPalindrome(a)); // true
+console.log(linkedListCycle(a));  // true
