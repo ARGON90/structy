6fc90fa96817fcc590b136ed57fbc3e058e820f2
argon <argonglach@gmail.com>
2023-01-31 13:18:51 -0500

    working on DSA

--- positions.js
+++ positions.js
@@ -1,24 +1,40 @@
+const parentheticalPossibilities = (s) => {
+  if (s.length === 0) return [''];
+
+  const allPossibilities = [];
+
+  const {choices, remainder} = getChoices(s);
+  console.log('first choices, string:', s, 'choices:', choices, remainder)
+  //a single choice is a character
+  for (let choice of choices) {
+    const remainderPossibilities = parentheticalPossibilities(remainder);
+    for (let substring of remainderPossibilities) {
+      allPossibilities.push(choice + substring)
+    }
+  };
+  console.log('return stmt', allPossibilities)
+  return allPossibilities;
+};
+
+//return one character from parenthesis, and return tail of sentence
+//format it as an object
 const getChoices = (s) => {
-  if (!s.length) return '';
-  if (!s.includes('(')) return s[0];
-
-  let res = '';
-  let pushChar = false;
-
-  for (let i = 0; i < s.length; i++) {
-    if (s[i] === ')') {
-      pushChar = false;
-    }
-    if (pushChar) {
-      res += s[i]
-    }
-    if (s[i] === '(') {
-      pushChar = true
+  if (s[0] === '(') {
+    let end = s.indexOf(')') //first index of this character's occurence
+    const choices = s.slice(1, end);
+    const remainder = s.slice(end + 1);
+    return {
+      choices,
+      remainder
+    };
+  } else {
+    return {
+    choices: s[0],
+    remainder: s.slice(1)
     }
   }
-
-  return res;
-
 }
 
-console.log(getChoices('get(choi)ces'))
+
+console.log(parentheticalPossibilities("(qr)ab(stu)c")); // ->
+// [ 'qabsc', 'qabtc', 'qabuc', 'rabsc', 'rabtc', 'rabuc' ]
