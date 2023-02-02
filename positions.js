c7a8d5d036d840257e21b7bfa6cf0db9f8edc5f1
argon <argonglach@gmail.com>
2023-02-02 00:30:06 -0500

    working on DSA

--- positions.js
+++ positions.js
@@ -1,61 +1,21 @@
-class Node {
-  constructor(val) {
-    this.val = val;
-    this.left = null;
-    this.right = null;
+function canColor(graph) {
+
+let set = new Set();
+let color = 'red';
+
+for (let node of graph) {
+  let currentNodes = graph[node];
+  for (let neighbor of currentNodes) {
+    if (set.has(neighbor)) continue;
+    
   }
+
 }
 
-const a = new Node('a');
-const b = new Node('b');
-const c = new Node('c');
-const d = new Node('d');
-const e = new Node('e');
-const f = new Node('f');
-const g = new Node('g');
-const h = new Node('h');
-
-a.left = b;
-a.right = c;
-b.left = d;
-b.right = e;
-c.right = f;
-e.left = g;
-e.right = h;
-
-const lowestCommonAncestor = (root, val1, val2) => {
-
-  let val1Path = recursiveTreeSum(root, val1);
-  let val2Path = recursiveTreeSum(root, val2);
-  console.log(val2Path, val1Path);
-  let set = new Set(val2Path)
-  for (let val of val1Path) {
-    if (set.has(val)) {
-      return val;
-    }
-  }
-  return val1Path;
-};
-
-const recursiveTreeSum = (root, targetVal) => {
-  if (root === null) return null;
-  console.log(root.val)
-  if (root.val === targetVal) return [targetVal];
-
-  const leftPath = recursiveTreeSum(root.left, targetVal);
-  const rightPath = recursiveTreeSum(root.left, targetVal);
-
-
-  if (leftPath !== null) {
-    leftPath.push(root.val)
-    return leftPath
-  }
-  if (rightPath !== null) {
-    rightPath.push(root.val)
-    return rightPath
-  }
-
-  return null;
 }
 
-console.log(lowestCommonAncestor(a, 'd', 'h')); // b)
+canColor({
+  x: ["y"],
+  y: ["x","z"],
+  z: ["y"]
+}); // -> true
