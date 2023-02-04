0b2a8160a7a2486da6718b09e7f9ad77eeb1ab6e
argon <argonglach@gmail.com>
2023-02-04 13:08:39 -0500

    DSA practice

--- practice.js
+++ practice.js
@@ -1,30 +1,15 @@
-const compress = (str) => {
-    let res = [];
-    let i = 0;
-    let j = 0;
+const pairSum = (nums, sum) => {
 
-    while (j <= str.length) {
-        if (str[i] !== str[j]) {
-            let num = j - i;
-            if (num === 1) {
-                res.push(str[i])
-            } else {
-                console.log(typeof num)
-                res.push(num, str[i])
-            }
-            i = j
-        } else {
-            j++
+    let prevNums = {};
+
+    for (let i = 0; i < nums.length; i++) {
+        let currentNum = nums[i];
+        let complement = sum / currentNum;
+        if (complement in prevNums) {
+            return [prevNums[complement], i];
         }
-
+        prevNums[currentNum] = i;
     }
-    return res.join('');
-};
+}
 
-
-console.log(compress("ssssbbz"));
-
-let arr = []
-arr.push(3, 5)
-arr.push('k', 3)
-// console.log(arr)
+console.log(pairSum([3, 2, 5, 4, 1], 8)); // -> [0, 2]
