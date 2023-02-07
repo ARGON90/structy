dd1cb7f956f7efe7abd36e6cc734c015e9149812
argon <argonglach@gmail.com>
2023-02-07 00:24:07 -0500

    DSA practice

--- practice.js
+++ practice.js
@@ -1,27 +1,9 @@
-const fiveSort = (numbers) => {
-    let i = 0;
-    let j = numbers.length - 1;
-
-    while (j > i) {
-
-        //if i is a 5
-        if (numbers[i] === 5) {
-
-            //and j is NOT a 5, switch them
-            if (numbers[j] !== 5) {
-                [numbers[i], numbers[j]] = [numbers[j], numbers[i]];
-            }
-            j--
-        } else {
-            i++
-        }
-        //i is NOT a 5
-
-    }
-    return numbers;
-
+function getJSONDiff(json1, json2) {
+    // const jsonArray1 = json1.split(',').join('').split(':').join('').split('"');
+    // const jsonArray2 = json1.split(',').join('').split(':').join('').split('"');
+    const jsonArr1 = JSON.parse(json1)
+    return jsonArr1
 }
-console.log(fiveSort([5, 2, 5, 6, 5, 1, 10, 2, 5, 5]));
-// -> [2, 2, 10, 6, 1, 5, 5, 5, 5, 5]
 
 
+console.log(getJSONDiff(['hello:world,hi:world'], []))
