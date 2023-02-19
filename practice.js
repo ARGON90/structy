482757752e8bceea33aa39e135e450d367b45d02
argon <argonglach@gmail.com>
2023-02-18 21:48:21 -0500

    dsa practice

--- practice.js
+++ practice.js
@@ -1,9 +1,1 @@
-function getJSONDiff(json1, json2) {
-    // const jsonArray1 = json1.split(',').join('').split(':').join('').split('"');
-    // const jsonArray2 = json1.split(',').join('').split(':').join('').split('"');
-    const jsonArr1 = JSON.parse(json1)
-    return jsonArr1.result
-}
 
-
-console.log(getJSONDiff('{"result":true, "count":42}', []))
