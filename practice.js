2610885ab0aa51bcfb85734f43fcd64642f1f41d
argon <argonglach@gmail.com>
2023-02-19 16:38:23 -0500

    dsa practice

--- practice.js
+++ practice.js
@@ -1,1 +1,20 @@
 
+
+const maxPair =  (nums) => {
+    let i = 0;
+    let j = 1;
+    let max = -Infinity;
+    let res;
+    while (j < nums.length) {
+        let currentSum = nums[i] + nums[j]
+        if (currentSum > max) {
+            res = [i, j]
+            max = currentSum
+        }
+        i++;
+        j++
+    }
+    return res;
+}
+
+maxPair([0, 4, 3, 1, 2, 3, 4, 0])
