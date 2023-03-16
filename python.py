6eb27db22eec81946e739714b58eee004adf5602
argon <argonglach@gmail.com>
2023-03-16 17:20:07 -0400

    dsa practice

---
+++ python.py
@@ -0,0 +1,13 @@
+def uncompress(s):
+    nums = '0123456789'
+    res = ''
+
+    for char in s:
+        i = 0
+        if char in nums:
+            print(int(char))
+
+    return res
+
+
+print(uncompress("2c3a1t"))  # -> 'ccaaat'
